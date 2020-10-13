e8e9e15ddbc5d95f1de50962c3c4bd820d2b0917
Brett Terpstra <me@brettterpstra.com>
2020-10-13 09:46:29 -0500

    updating web excursions script

--- webexcursions.rb
+++ webexcursions.rb
@@ -21,19 +21,19 @@ config = {
 }
 
 # template for post headers
-config[:post_template] = <<-ENDTEMPLATE
----
-title: "%%TYPE%% links for #{Time.now.strftime('%B %-d, %Y')}"
-layout: post
-tags:
-- bookmarks
-- %%TYPE_TAG%%
-categories:
-- Bookmarks
-post_class: bookmarks
-description: "The latest links from around the web."
-comments: false
----
+config[:post_template] = <<~ENDTEMPLATE
+  ---
+  title: "%%TYPE%% links for #{Time.now.strftime('%B %-d, %Y')}"
+  layout: post
+  tags:
+  - bookmarks
+  - %%TYPE_TAG%%
+  categories:
+  - Bookmarks
+  post_class: bookmarks
+  description: "The latest links from around the web."
+  comments: false
+  ---
 
 ENDTEMPLATE
 
@@ -67,8 +67,9 @@ config[:blog_tag] = case config[:type]
                       '.natblogit'
                     end
 
+# SSL Fix
 class Net::HTTP
-  alias_method :old_initialize, :initialize
+  alias old_initialize initialize
 
   def initialize(*args)
     old_initialize(*args)
@@ -79,33 +80,33 @@ end
 
 # Pinboard methods
 class Pinboard
-  attr_accessor :existing_bookmarks, :new_bookmarks
+  attr_accessor :existing_bookmarks
   def initialize(config)
     @config = config
     # Make storage directory if needed
-    FileUtils.mkdir_p(@config[:db_location], :mode => 0755) unless File.exist? @config[:db_location]
+    FileUtils.mkdir_p(@config[:db_location], { mode: 0o755 }) unless File.exist? @config[:db_location]
 
     # load existing bookmarks database
     @existing_bookmarks = read_bookmarks
   end
 
-  def debug_msg(message,sticky = true)
-    if @config[:debug]
-      $stderr.puts message
-    end
+  def debug_msg(message)
+    warn message if @config[:debug]
   end
 
   # Store a Marshal dump of a hash
-  def store obj = @existing_bookmarks, file_name = @config[:db_location]+"/#{@config[:type]}-bookmarks.stash", options={:gzip => false }
-    marshal_dump = Marshal.dump(obj)
-    file = File.new(file_name,'w')
-    file = Zlib::GzipWriter.new(file) unless options[:gzip] == false
+  def store(opts = {})
+    opts[:obj] ||= @existing_bookmarks
+    opts[:file_name] ||= @config[:db_location] + "/#{@config[:type]}-bookmarks.stash"
+    opts[:options] ||= { gzip: false }
+    marshal_dump = Marshal.dump(opts[:obj])
+    file = opts[:options][:gzip] ? Zlib::GzipWriter.new(opts[:file_name]) : File.new(opts[:file_name], 'w')
     file.write marshal_dump
     file.close
-    return obj
   end
+
   # Load the Marshal dump to a hash
-  def load file_name
+  def load(file_name)
     begin
       file = Zlib::GzipReader.open(file_name)
     rescue Zlib::GzipFile::Error
@@ -113,16 +114,12 @@ class Pinboard
     ensure
       obj = Marshal.load file.read
       file.close
-      return obj
     end
+    obj
   end
 
   def new_bookmarks
-     return self.unique_bookmarks
-  end
-
-  def existing_bookmarks
-    @existing_bookmarks
+    unique_bookmarks
   end
 
   def add_auth(api_call)
@@ -135,51 +132,52 @@ class Pinboard
     request = Net::HTTP.new('api.pinboard.in', 443)
     request.use_ssl = true
     request.start do |http|
-    	req = Net::HTTP::Get.new(add_auth(api_call))
-    	response = http.request(req)
-    	response.value
-    	xml = response.body
+      req = Net::HTTP::Get.new(add_auth(api_call))
+      response = http.request(req)
+      response.value
+      xml = response.body
     end
-    return REXML::Document.new(xml)
+    REXML::Document.new(xml)
   end
+
   # converts Pinboard API output to an array of URLs
   def bookmarks_to_array(doc)
     bookmarks = []
     doc.elements.each('posts/post') do |ele|
       post = {}
-      ele.attributes.each {|key,val|
-        post[key] = val;
-      }
+      ele.attributes.each { |key, val| post[key] = val }
       bookmarks.push(post)
     end
-    return bookmarks
+    bookmarks
   end
+
   # compares bookmark array to existing bookmarks to find new urls
   def unique_bookmarks
-      xml = self.get_xml("/v1/posts/recent?tag=#{@config[:blog_tag]}&count=100")
-      bookmarks = self.bookmarks_to_array(xml)
-      unless @existing_bookmarks.nil?
-        old_hrefs = @existing_bookmarks.map { |x| x['href'] }
-        bookmarks.reject! { |s| old_hrefs.include? s['href'] }
-      end
-      return bookmarks
+    xml = get_xml("/v1/posts/recent?tag=#{@config[:blog_tag]}&count=100")
+    bookmarks = bookmarks_to_array(xml)
+    unless @existing_bookmarks.nil?
+      old_hrefs = @existing_bookmarks.map { |x| x['href'] }
+      bookmarks.reject! { |s| old_hrefs.include? s['href'] }
+    end
+    bookmarks
   end
+
   # wrapper for load
   def read_bookmarks
     # if the file exists, read it
-    if File.exists? @config[:db_location]+"/#{@config[:type]}-bookmarks.stash"
-      return self.load @config[:db_location]+"/#{@config[:type]}-bookmarks.stash"
+    if File.exist? File.join(@config[:db_location], "#{@config[:type]}-bookmarks.stash")
+      self.load File.join(@config[:db_location], "#{@config[:type]}-bookmarks.stash")
     else # new database
-      return []
+      []
     end
   end
 
   def read_current_excursion
     # if the file exists, read it
-    if File.exists? @config[:db_location]+"/#{@config[:type]}-current.stash"
-      return self.load @config[:db_location]+"/#{@config[:type]}-current.stash"
+    if File.exist? File.join(@config[:db_location], "#{@config[:type]}-current.stash")
+      self.load(File.join(@config[:db_location], "#{@config[:type]}-current.stash"))
     else # new database
-      return []
+      []
     end
   end
 end
@@ -191,44 +189,42 @@ new_bookmarks = pb.new_bookmarks
 
 # load the current draft stash
 current_excursion = pb.read_current_excursion
-if new_bookmarks.count > 0 || current_excursion.count > 0
-  pb.debug_msg("Found #{new_bookmarks.count} unindexed bookmarks.",false)
+if new_bookmarks.count.positive? || current_excursion.count.positive?
+  pb.debug_msg("Found #{new_bookmarks.count} unindexed bookmarks.")
 else
-  pb.debug_msg("No new bookmarks. Exiting.",false)
+  pb.debug_msg('No new bookmarks. Exiting.')
   exit
 end
 
 # merge new bookmarks into main database
 existing_hrefs = current_excursion.map { |x| x['href'] }
-new_bookmarks.each {|bookmark|
+new_bookmarks.each do |bookmark|
   pb.existing_bookmarks.push(bookmark)
-  unless existing_hrefs.include? bookmark['href']
-    current_excursion.push(bookmark)
-  end
-}
+  current_excursion.push(bookmark) unless existing_hrefs.include? bookmark['href']
+end
 
 pb.store
 
 # if there are at least min_count bookmarks, create a draft post and clear cache
 if current_excursion.length >= config[:min_count].to_i || config[:force]
   if current_excursion.length > config[:min_count].to_i && !config[:force]
-    excursions = current_excursion.slice!(0,config[:min_count])
+    excursions = current_excursion.slice!(0, config[:min_count])
   else
     excursions = current_excursion
     current_excursion = []
   end
 
-  output = config[:post_template].gsub(/%%TYPE%%/, config[:type].capitalize).gsub(/%%TYPE_TAG%%/, config[:type].downcase)
+  output = config[:post_template]
+           .gsub(/%%TYPE%%/, config[:type].capitalize)
+           .gsub(/%%TYPE_TAG%%/, config[:type].downcase)
 
-  excursions.each_with_index {|bookmark, i|
+  excursions.each do |bookmark|
     output += "[#{bookmark['description']}](#{bookmark['href']})\n"
-    output += ": #{bookmark['extended'].gsub(/\n+/,' ')}\n\n"
-  }
+    output += ": #{bookmark['extended'].gsub(/\n+/, ' ')}\n\n"
+  end
 
   draft = "#{config[:drafts_folder]}/#{config[:type]}-web-excursions-#{Time.now.strftime('%B-%d-%Y-1').downcase}"
-  while (File.exist?(draft + '.md'))
-    draft.next!
-  end
+  draft.next! while File.exist? draft + '.md'
   draft += '.md'
 
   File.open(draft, 'w+') do |f|
@@ -247,4 +243,6 @@ current_excursion.each_with_index do |bookmark, i|
   puts "#{idx}: #{bookmark['description']}"
 end
 
-pb.store(current_excursion, config[:db_location]+"/#{config[:type]}-current.stash", options={ gzip: false })
+pb.store({  obj: current_excursion,
+            file_name: config[:db_location] + "/#{config[:type]}-current.stash",
+            options: { gzip: false } })
